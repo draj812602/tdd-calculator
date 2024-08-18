a192276986a92d14cb98d75a5d76559aa35a0463
dheeraj kumar <dheerajkumar@192.168.1.3>
2024-08-19 00:12:51 +0530

    used regex to handle the new line

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,8 @@
 export function add(numbers) {
   if (numbers === "") return 0;
+  const delimiters = [",", "\n"];
+  const regex = new RegExp(`[${delimiters.join("")}]`, "g");
   // converting string to integer and then calculating the sum using reduce
-  const numberArray = numbers.split(",").map((num) => parseInt(num, 10));
+  const numberArray = numbers.split(regex).map((num) => parseInt(num, 10));
   return numberArray.reduce((sum, num) => sum + num, 0);
 }
