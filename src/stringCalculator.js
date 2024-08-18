361349064753d943dd062e881a365eaaa0cec0db
dheeraj kumar <dheerajkumar@192.168.1.3>
2024-08-19 00:09:38 +0530

    Passed for two numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,4 +1,6 @@
 export function add(numbers) {
   if (numbers === "") return 0;
-  return parseInt(numbers, 10);
+  // converting string to integer and then calculating the sum using reduce
+  const numberArray = numbers.split(",").map((num) => parseInt(num, 10));
+  return numberArray.reduce((sum, num) => sum + num, 0);
 }
