17f9da41b7adbfc02079e71b2b4fdeb43bece8b2
dheeraj kumar <dheerajkumar@192.168.1.3>
2024-08-19 00:24:45 +0530

    const regex = new RegExp(, 'g'); this was causing issue and because of it split was also not working

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,14 +1,16 @@
 export function add(numbers) {
   if (numbers === "") return 0;
-  let seperators = /[,\n]/;
+  let delimiters = [",", "\n"];
   if (numbers.startsWith("//")) {
-    const seperatorsLineEnd = numbers.indexOf("\n");
-    seperators = numbers[2];
-    numbers = numbers.substring(seperatorsLineEnd + 1);
+    const delimiterLineEnd = numbers.indexOf("\n");
+    const customDelimiter = numbers.substring(2, delimiterLineEnd);
+    delimiters.push(customDelimiter);
+    numbers = numbers.substring(delimiterLineEnd + 1);
   }
 
-  const regex = new RegExp(`[${seperators}]`, "g");
-  // converting string to integer and then calculating the sum using reduce
-  const numberArray = numbers.split(regex).map((num) => parseInt(num, 10));
+  const numberArray = numbers
+    .split(new RegExp(`[${delimiters.join("")}]`))
+    .map((num) => parseInt(num, 10));
+
   return numberArray.reduce((sum, num) => sum + num, 0);
 }
