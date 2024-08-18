efda569e1d1fe79a7537abc5fc1cf11bc2fd5fba
dheeraj kumar <dheerajkumar@192.168.1.3>
2024-08-19 00:21:40 +0530

    again test case failed, this time unwanted / unknown

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,13 @@
 export function add(numbers) {
   if (numbers === "") return 0;
-  const delimiters = [",", "\n"];
-  const regex = new RegExp(`[${delimiters.join("")}]`, "g");
+  let seperators = /[,\n]/;
+  if (numbers.startsWith("//")) {
+    const seperatorsLineEnd = numbers.indexOf("\n");
+    seperators = numbers[2];
+    numbers = numbers.substring(seperatorsLineEnd + 1);
+  }
+
+  const regex = new RegExp(`[${seperators}]`, "g");
   // converting string to integer and then calculating the sum using reduce
   const numberArray = numbers.split(regex).map((num) => parseInt(num, 10));
   return numberArray.reduce((sum, num) => sum + num, 0);
